a3cdb4e604d4cef325b0b9ec2358aef83a2288bd
Yadakk <lolyer47@gmail.com>
2024-09-24 16:00:28 +0300

    init: Init

---
+++ Assets/Source/GameAssembly/Core/EntryPoint.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceInvadersTask.Core
+{
+    public class EntryPoint : MonoBehaviour
+    {
+
+    }
+}
