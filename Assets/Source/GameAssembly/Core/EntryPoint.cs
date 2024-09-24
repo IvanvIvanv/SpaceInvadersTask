e968ff0a5a2d7bb42caa4b0dc74115b689610a14
Yadakk <lolyer47@gmail.com>
2024-09-24 16:21:15 +0300

    feat: Added assembly definition for game

--- Assets/Source/GameAssembly/Core/EntryPoint.cs
+++ Assets/Source/GameAssembly/Core/EntryPoint.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace SpaceInvadersTask.Core
+namespace SpaceInvadersTask.GameAssembly
 {
     public class EntryPoint : MonoBehaviour
     {
